881ebb7f991c0e7fb8f6e944b035b84871728c7f
chao87119 <chaoshihshan@gamil.com>
2020-03-25 11:29:05 +0800

    Update script.js

--- cal/dist/script.js
+++ cal/dist/script.js
@@ -29,7 +29,7 @@ var vm = new Vue({
     "name": "牛肉",
     "amount":"",
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/cow.jpg",
+    "cover": "/ecow/cal/img/cow.jpg",
     "description": "碳排放量:",
     "price": 0.00921,
     "open": false
@@ -38,7 +38,7 @@ var vm = new Vue({
     "name": "豬肉",
     "amount":"",
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/pig.jpg",
+    "cover": "/ecow/cal/img/pig.jpg",
     "description": "碳排放量:",
     "price": 0.00216,
     "open": false
@@ -47,7 +47,7 @@ var vm = new Vue({
     "name": "雞肉",
     "amount":"",
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/chicken.jpg",
+    "cover": "/ecow/cal/img/chicken.jpg",
     "description": "碳排放量:",
     "price": 0.00123,
     "open": false
@@ -56,7 +56,7 @@ var vm = new Vue({
     "name": "雞蛋",
     "amount":"",      
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/egg.jpeg",
+    "cover": "/ecow/cal/img/egg.jpeg",
     "description": "碳排放量:",
     "price": 0.00107,
     "open": false
@@ -64,7 +64,7 @@ var vm = new Vue({
     "name": "牛奶",
     "amount":"",
     "type": "奶類",
-    "cover": "/cal/img/milk.jpg",
+    "cover": "/ecow/cal/img/milk.jpg",
     "description": "碳排放量:",
     "price": 0.000139,
     "open": false
@@ -72,7 +72,7 @@ var vm = new Vue({
     "name": "魚",
     "amount":"",
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/fish.jpg",
+    "cover": "/ecow/cal/img/fish.jpg",
     "description": "碳排放量:",
     "price": 0.001926,
     "open": false
@@ -80,7 +80,7 @@ var vm = new Vue({
     "name": "蝦",
     "amount":"",
     "type": "蛋豆魚肉類",
-    "cover": "/cal/img/shrimp.jpg",
+    "cover": "/ecow/cal/img/shrimp.jpg",
     "description": "碳排放量:",
     "price": 0.002938,
     "open": false
@@ -88,7 +88,7 @@ var vm = new Vue({
     "name": "蔬菜",
     "amount":"",
     "type": "蔬菜類",
-    "cover": "/cal/img/vegetable.jpg",
+    "cover": "/ecow/cal/img/vegetable.jpg",
     "description": "碳排放量:",
     "price": 0.000355,
     "open": false
@@ -96,7 +96,7 @@ var vm = new Vue({
     "name": "水果",
     "amount":"",
     "type": "水果類",
-    "cover": "/cal/img/fruit.jpg",
+    "cover": "/ecow/cal/img/fruit.jpg",
     "description": "碳排放量:",
     "price": 0.000234,
     "open": false
@@ -104,7 +104,7 @@ var vm = new Vue({
     "name": "米飯",
     "amount":"",
     "type": "五穀根莖類",
-    "cover": "/cal/img/rice.jpg",
+    "cover": "/ecow/cal/img/rice.jpg",
     "description": "碳排放量:",
     "price": 0.000099,
     "open": false
@@ -113,7 +113,7 @@ var vm = new Vue({
     "name": "澱粉類",
     "amount":"",      
     "type": "澱粉類",
-    "cover": "/cal/img/澱粉.jpg",
+    "cover": "/ecow/cal/img/澱粉.jpg",
     "description": "碳排放量:",
     "price": 0.000335,
     "open": false
@@ -122,7 +122,7 @@ var vm = new Vue({
     "name": "其他食物",
     "amount":"",  
     // "type": "蛋豆魚肉類",
-    "cover": "/cal/img/otherfood.png",
+    "cover": "/ecow/cal/img/otherfood.png",
     "description": "碳排放量:",
     // "price": 9.21,
     "open": false
@@ -133,7 +133,7 @@ var vm = new Vue({
     "name": "冷氣機",
     "amount":"",
     "type": "消耗電力 900w</br>每年使用時間估計 900小時</br>年耗電量 810度<br/>",
-    "cover": "/cal/img/冷氣機.jpeg",
+    "cover": "/ecow/cal/img/冷氣機.jpeg",
     "description": "碳排放量:",
     "price": 492.72,
     "open": false
@@ -142,7 +142,7 @@ var vm = new Vue({
     "name": "吹風機",
     "amount":"", 
     "type": "消耗電力 800w</br>每年使用時間估計 30小時</br>年耗電量 24度<br/>",
-    "cover": "/cal/img/吹風機.webp",
+    "cover": "/ecow/cal/img/吹風機.webp",
     "description": "碳排放量:",
     "price": 14.69,
     "open": false
@@ -151,7 +151,7 @@ var vm = new Vue({
     "name": "電暖爐",
     "amount":"",
     "type": "消耗電力 700w</br>每年使用時間估計 270小時</br>年耗電量 189度<br/>",
-    "cover": "/cal/img/電暖爐.jpg",
+    "cover": "/ecow/cal/img/電暖爐.jpg",
     "description": "碳排放量:",
     "price": 115.67,
     "open": false
@@ -160,7 +160,7 @@ var vm = new Vue({
     "name": "除濕機",
     "amount":"",
     "type": "消耗電力 285w</br>每年使用時間估計 540小時</br>年耗電量 153.9度<br/>",
-    "cover": "/cal/img/除濕機.jpg",
+    "cover": "/ecow/cal/img/除濕機.jpg",
     "description": "碳排放量:",
     "price": 94.19,
     "open": false
@@ -169,7 +169,7 @@ var vm = new Vue({
     "name": "電扇",
     "amount":"",
     "type": "消耗電力 66w</br>每年使用時間估計 720小時</br>年耗電量 47.5度<br/>",
-    "cover": "/cal/img/電扇.jpg",
+    "cover": "/ecow/cal/img/電扇.jpg",
     "description": "碳排放量:",
     "price": 29.07,
     "open": false
@@ -177,7 +177,7 @@ var vm = new Vue({
     "name": "抽風機",
     "amount":"",
     "type": "消耗電力 30w</br>每年使用時間估計 480小時</br>年耗電量 14.4度<br/>",
-    "cover": "/cal/img/抽風機.jpeg",
+    "cover": "/ecow/cal/img/抽風機.jpeg",
     "description": "碳排放量:",
     "price": 8.81,
     "open": false
@@ -185,7 +185,7 @@ var vm = new Vue({
     "name": "燈泡",
     "amount":"",
     "type": "消耗電力 60w</br>每年使用時間估計 1080小時</br>年耗電量 64.8度<br/>",
-    "cover": "/cal/img/燈泡.jpg",
+    "cover": "/ecow/cal/img/燈泡.jpg",
     "description": "碳排放量:",
     "price": 39.66,
     "open": false
@@ -193,7 +193,7 @@ var vm = new Vue({
     "name": "日光燈",
     "amount":"",
     "type": "消耗電力 25w</br>每年使用時間估計 1800小時</br>年耗電量 45度<br/>",
-    "cover": "/cal/img/日光燈.jpeg",
+    "cover": "/ecow/cal/img/日光燈.jpeg",
     "description": "碳排放量:",
     "price": 27.54,
     "open": false
@@ -201,7 +201,7 @@ var vm = new Vue({
     "name": "省電燈泡",
     "amount":"",
     "type": "消耗電力 17w</br>每年使用時間估計 1800小時</br>年耗電量 30.6度<br/>",
-    "cover": "/cal/img/省電燈泡.jpg",
+    "cover": "/ecow/cal/img/省電燈泡.jpg",
     "description": "碳排放量:",
     "price": 18.73,
     "open": false
@@ -209,7 +209,7 @@ var vm = new Vue({
     "name": "微波爐",
     "amount":"",
     "type": "消耗電力 1200w</br>每年使用時間估計 60小時</br>年耗電量 72度<br/>",
-    "cover": "/cal/img/微波爐.png",
+    "cover": "/ecow/cal/img/微波爐.png",
     "description": "碳排放量:",
     "price": 44.06,
     "open": false
@@ -217,7 +217,7 @@ var vm = new Vue({
     "name": "電磁爐",
     "amount":"",
     "type": "消耗電力 1200w</br>每年使用時間估計 24小時</br>年耗電量 28.8度<br/>",
-    "cover": "/cal/img/電磁爐.jpg",
+    "cover": "/ecow/cal/img/電磁爐.jpg",
     "description": "碳排放量:",
     "price": 17.63,
     "open": false
@@ -226,7 +226,7 @@ var vm = new Vue({
     "name": "開飲機",
     "amount":"",
     "type": "消耗電力 800w</br>每年使用時間估計 120小時</br>年耗電量 576度<br/>",
-    "cover": "/cal/img/開飲機.jpg",
+    "cover": "/ecow/cal/img/開飲機.jpg",
     "description": "碳排放量:",
     "price": 352.51,
     "open": false
@@ -235,7 +235,7 @@ var vm = new Vue({
     "name": "電鍋",
     "amount":"",  
     "type": "消耗電力 800w</br>每年使用時間估計 180小時</br>年耗電量 144度<br/>",
-    "cover": "/cal/img/電鍋.jpg",
+    "cover": "/ecow/cal/img/電鍋.jpg",
     "description": "碳排放量:",
     "price": 88.13,
     "open": false
@@ -244,7 +244,7 @@ var vm = new Vue({
     "name": "電烤箱",
     "amount":"", 
     "type": "消耗電力 800w</br>每年使用時間估計 24小時</br>年耗電量 19.2度<br/>",
-    "cover": "/cal/img/電烤箱.jpg",
+    "cover": "/ecow/cal/img/電烤箱.jpg",
     "description": "碳排放量:",
     "price": 11.75,
     "open": false
@@ -253,7 +253,7 @@ var vm = new Vue({
     "name": "抽油煙機",
     "amount":"",
     "type": "消耗電力 350w</br>每年使用時間估計 120小時</br>年耗電量 42度<br/>",
-    "cover": "/cal/img/抽油煙機.jpeg",
+    "cover": "/ecow/cal/img/抽油煙機.jpeg",
     "description": "碳排放量:",
     "price": 25.7,
     "open": false
@@ -262,7 +262,7 @@ var vm = new Vue({
     "name": "果菜榨汁機",
     "amount":"", 
     "type": "消耗電力 210w</br>每年使用時間估計 12小時</br>年耗電量 2.5度<br/>",
-    "cover": "/cal/img/果菜榨汁機.jpg",
+    "cover": "/ecow/cal/img/果菜榨汁機.jpg",
     "description": "碳排放量:",
     "price": 1.53,
     "open": false
@@ -271,7 +271,7 @@ var vm = new Vue({
     "name": "烘碗機",
     "amount":"", 
     "type": "消耗電力 200w</br>每年使用時間估計 180小時</br>年耗電量 36度<br/>",
-    "cover": "/cal/img/烘碗機.jpeg",
+    "cover": "/ecow/cal/img/烘碗機.jpeg",
     "description": "碳排放量:",
     "price": 22.03,
     "open": false
@@ -280,7 +280,7 @@ var vm = new Vue({
     "name": "電冰箱",
     "amount":"", 
     "type": "消耗電力 130w</br>每年使用時間估計 8640小時</br>年耗電量 1123.2度<br/>",
-    "cover": "/cal/img/電冰箱.jpg",
+    "cover": "/ecow/cal/img/電冰箱.jpg",
     "description": "碳排放量:",
     "price": 687.4,
     "open": false
@@ -289,7 +289,7 @@ var vm = new Vue({
     "name": "烘衣機",
     "amount":"", 
     "type": "消耗電力 1200w</br>每年使用時間估計 33小時</br>年耗電量 39.6度<br/>",
-    "cover": "/cal/img/烘衣機.jpg",
+    "cover": "/ecow/cal/img/烘衣機.jpg",
     "description": "碳排放量:",
     "price": 24.24,
     "open": false
@@ -298,7 +298,7 @@ var vm = new Vue({
     "name": "電熨斗",
     "amount":"", 
     "type": "消耗電力 800w</br>每年使用時間估計 36小時</br>年耗電量 28.8度<br/>",
-    "cover": "/cal/img/電熨斗.jpg",
+    "cover": "/ecow/cal/img/電熨斗.jpg",
     "description": "碳排放量:",
     "price": 17.63,
     "open": false
@@ -307,7 +307,7 @@ var vm = new Vue({
     "name": "洗衣機",
     "amount":"", 
     "type": "消耗電力 420w</br>每年使用時間估計 60小時</br>年耗電量 25.2度<br/>",
-    "cover": "/cal/img/wm.jpg",
+    "cover": "/ecow/cal/img/wm.jpg",
     "description": "碳排放量:",
     "price": 15.42,
     "open": false
@@ -316,7 +316,7 @@ var vm = new Vue({
     "name": "電視",
     "amount":"", 
     "type": "消耗電力 140w</br>每年使用時間估計 1440小時</br>年耗電量 20.16度<br/>",
-    "cover": "/cal/img/tv.jpg",
+    "cover": "/ecow/cal/img/tv.jpg",
     "description": "碳排放量:",
     "price": 12.34,
     "open": false
@@ -325,7 +325,7 @@ var vm = new Vue({
     "name": "音響",
     "amount":"", 
     "type": "消耗電力 50w</br>每年使用時間估計 360小時</br>年耗電量 18度<br/>",
-    "cover": "/cal/img/音響.jpg",
+    "cover": "/ecow/cal/img/音響.jpg",
     "description": "碳排放量:",
     "price": 12.34,
     "open": false
@@ -334,7 +334,7 @@ var vm = new Vue({
     "name": "其他電器",
     "amount":"",  
     // "type": "蛋豆魚肉類",
-    "cover": "/cal/img/otherapp.png",
+    "cover": "/ecow/cal/img/otherapp.png",
     "description": "碳排放量:",
     // "price": 9.21,
     "open": false
@@ -345,7 +345,7 @@ var vm = new Vue({
     "name": "汽車",
     "amount":"",
     "type": "1KM",
-    "cover": "/cal/img/car.jpg",
+    "cover": "/ecow/cal/img/car.jpg",
     "description": "碳排放量:",
     "price": 0.22,
     "open": false
@@ -354,7 +354,7 @@ var vm = new Vue({
     "name": "機車",
     "amount":"",
     "type": "1KM",
-    "cover": "/cal/img/motor.jpg",
+    "cover": "/ecow/cal/img/motor.jpg",
     "description": "碳排放量:",
     "price": 0.055,
     "open": false
@@ -371,7 +371,7 @@ var vm = new Vue({
     "name": "捷運",
     "amount":"",
     "type": "1KM",
-    "cover": "/cal/img/mrt.jpg",
+    "cover": "/ecow/cal/img/mrt.jpg",
     "description": "碳排放量:",
     "price": 0.07,
     "open": false
@@ -379,7 +379,7 @@ var vm = new Vue({
     "name": "公車",
     "amount":"",
     "type": "1KM",
-    "cover": "/cal/img/bus.jpg",
+    "cover": "/ecow/cal/img/bus.jpg",
     "description": "碳排放量:",
     "price": 0.08,
     "open": false
@@ -387,7 +387,7 @@ var vm = new Vue({
     "name": "汽油",
     "amount":"",
     "type": "1L",
-    "cover": "/cal/img/gas.jpg",
+    "cover": "/ecow/cal/img/gas.jpg",
     "description": "碳排放量:",
     "price": 2.24,
     "open": false
@@ -395,7 +395,7 @@ var vm = new Vue({
     "name": "柴油",
     "amount":"",
     "type": "1L",
-    "cover": "/cal/img/柴油.jpg",
+    "cover": "/ecow/cal/img/柴油.jpg",
     "description": "碳排放量:",
     "price": 2.7,
     "open": false
@@ -404,7 +404,7 @@ var vm = new Vue({
     "name": "其他交通工具",
     "amount":"",  
     // "type": "蛋豆魚肉類",
-    "cover": "/cal/img/其他交通.jpeg",
+    "cover": "/ecow/cal/img/其他交通.jpeg",
     "description": "碳排放量:",
     // "price": 9.21,
     "open": false
